b4d0fb53c50c890245cda6157df6c0beaa5feda6
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-04-02 14:30:52 -0400

    added in player info

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -9,11 +9,26 @@ const Player = require("../lib/Player");
 test("creates a player object", () => {
   const player = new Player("Dave");
 
-  expect(player.inventory).toEqual(
-    expect.arrayContaining([expect.any(Object)])
-  );
   expect(player.name).toBe("Dave");
   expect(player.health).toEqual(expect.any(Number));
   expect(player.strength).toEqual(expect.any(Number));
   expect(player.agility).toEqual(expect.any(Number));
+});
+
+test("gets player's stats as an object", () => {
+  const player = new Player("Dave");
+
+  expect(player.getStats()).toHaveProperty("potions");
+  expect(player.getStats()).toHaveProperty("health");
+  expect(player.getStats()).toHaveProperty("agility");
+});
+
+test("gets inventory from player or returns false", () => {
+  const player = new Player("Dave");
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
 });
